2eb546c4adb52c4ab3e0730adfb3545f71a8470c
Igor de Souza Bezerra <igorsouzabezerra@outlook.com>
2022-02-19 17:21:53 -0300

    ensure ExpressRouter responds with correct statusCode and error

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -9,7 +9,11 @@ class ExpressRouter {
 
   async adapt (req: Request, res: Response): Promise<void> {
     const httpResponse = await this.controller.handle({ ...req.body })
-    res.status(200).json(httpResponse.data)
+    if (httpResponse.statusCode === 200) {
+      res.status(200).json(httpResponse.data)
+    } else {
+      res.status(httpResponse.statusCode).json({ error: httpResponse.data.message })
+    }
   }
 }
 
@@ -50,8 +54,22 @@ describe('ExpressRouter', () => {
     await sut.adapt(req, res)
 
     expect(res.status).toHaveBeenCalledWith(200)
-    expect(res.json).toHaveBeenCalledWith({ data: 'any_data' })
     expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ data: 'any_data' })
+    expect(res.json).toHaveBeenCalledTimes(1)
+  })
+
+  it('should respond with 400 and valid error', async () => {
+    controller.handle.mockResolvedValueOnce({
+      statusCode: 400,
+      data: new Error('any_error')
+    })
+
+    await sut.adapt(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
     expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
