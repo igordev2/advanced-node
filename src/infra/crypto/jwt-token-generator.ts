b5a9929c2c7f49e5aba7785416379c8f1437cc02
Igor de Souza Bezerra <igorsouzabezerra@outlook.com>
2022-02-19 18:12:04 -0300

    improve code quality

--- src/infra/crypto/jwt-token-generator.ts
+++ src/infra/crypto/jwt-token-generator.ts
@@ -2,11 +2,14 @@ import { TokenGenerator } from '@/data/contracts/crypto'
 
 import { sign } from 'jsonwebtoken'
 
+type Params = TokenGenerator.Params
+type Result = TokenGenerator.Result
+
 export class JwtTokenGenerator implements TokenGenerator {
   constructor (private readonly secret: string) { }
 
-  async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
-    const expirationInMs = params.expirationInMs / 1000
-    return sign({ key: params.key }, this.secret, { expiresIn: expirationInMs })
+  async generateToken ({ key, expirationInMs }: Params): Promise<Result> {
+    const expirationInSeconds = expirationInMs / 1000
+    return sign({ key: key }, this.secret, { expiresIn: expirationInSeconds })
   }
 }
--- src/infra/postgres/repository/user-account.ts
+++ src/infra/postgres/repository/user-account.ts
@@ -3,11 +3,16 @@ import { PgUser } from '@/infra/postgres/entities'
 
 import { getRepository } from 'typeorm'
 
+type LoadParams = LoadUserAccountRepository.Params
+type LoadResult = LoadUserAccountRepository.Result
+type SaveParams = SaveFacebookAccountRepository.Params
+type SaveResult = SaveFacebookAccountRepository.Result
+
 export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
   private readonly pgUserRepo = getRepository(PgUser)
 
-  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUser = await this.pgUserRepo.findOne({ email: params.email })
+  async load ({ email }: LoadParams): Promise<LoadResult> {
+    const pgUser = await this.pgUserRepo.findOne({ email })
     if (pgUser !== undefined) {
       return {
         id: pgUser?.id.toString(),
@@ -16,25 +21,16 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
     }
   }
 
-  async saveWithFacebook (params: SaveFacebookAccountRepository.Params): Promise<SaveFacebookAccountRepository.Result> {
-    let id: string
-    if (params.id === undefined) {
-      const pgUser = await this.pgUserRepo.save({
-        email: params.email,
-        name: params.name,
-        facebookId: params.facebookId
-      })
-      id = pgUser.id.toString()
+  async saveWithFacebook ({ id, name, email, facebookId }: SaveParams): Promise<SaveResult> {
+    let resultId: string
+    if (id === undefined) {
+      const pgUser = await this.pgUserRepo.save({ email, name, facebookId })
+      resultId = pgUser.id.toString()
     } else {
-      id = params.id
-      await this.pgUserRepo.update({
-        id: parseInt(params.id)
-      }, {
-        name: params.name,
-        facebookId: params.facebookId
-      })
+      resultId = id
+      await this.pgUserRepo.update({ id: parseInt(id) }, { name, facebookId })
     }
 
-    return { id }
+    return { id: resultId }
   }
 }
