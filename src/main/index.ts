506b772450500a8ef91913b93622bc1d5ded0e40
Igor de Souza Bezerra <igorsouzabezerra@outlook.com>
2022-02-22 16:00:04 -0300

    move typeorm config to root folder

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,8 +4,7 @@ import { env } from '@/main/config/env'
 
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-import { config } from '@/infra/postgres/helpers'
 
-createConnection(config)
+createConnection()
   .then(() => app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`)))
   .catch(console.error)
