008ac48375e4ccfd3af6d5a012a2e588efaa85cf
Igor de Souza Bezerra <igorsouzabezerra@outlook.com>
2022-02-23 11:18:33 -0300

    ensure Authorize calls TokenValidatpr with correct params

--- src/domain/usecases/facebook-authentication.ts
+++ src/domain/usecases/facebook-authentication.ts
@@ -6,7 +6,9 @@ import { TokenGenerator } from '../contracts/crypto'
 
 type Setup = (
   facebookApi: LoadFacebookUserApi, userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository, crypto: TokenGenerator) => FacebookAuthentication
-export type FacebookAuthentication = (params: { token: string }) => Promise<{ accessToken: string }>
+type Input = { token: string }
+type OutPut = { accessToken: string }
+export type FacebookAuthentication = (params: Input) => Promise<OutPut>
 
 export const setupFacebookAuthentication: Setup = (facebookApi, userAccountRepo, crypto) => async params => {
   const fbData = await facebookApi.loadUser(params)
